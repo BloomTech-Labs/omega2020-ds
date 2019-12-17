392e57322d715b667cfa93623054af61aa892ad3
Rob Hamilton <RobHamilton3rd@gmail.com>
2019-12-17 14:18:34 -0500

    Converting Luna's csv to a DB file

--- Omega2020/schema.py
+++ Omega2020/schema.py
@@ -3,6 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 DB = SQLAlchemy()
 
 #A minimal Database with two values: ID for a Puzzle and the values that puzzle holds.
-class Puzzle(DB.Model):
+class PuzzleTable(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
-    values = DB.Column(DB.String(200), nullable=False)
+    sudoku = DB.Column(DB.String(200), nullable=False)
+    solution = DB.Column(DB.String(200), nullable=False)
+    level = DB.Column(DB.String(200), nullable=False)
+    people = DB.Column(DB.Integer, nullable=False)
+    avg_time = DB.Column(DB.String(500), nullable=False)
+    sudoku_hash = DB.Column(DB.String(100), nullable=False)
+
