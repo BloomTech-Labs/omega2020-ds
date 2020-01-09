eaf1e6bffdc2728877e60c7cc51bdde5eb357244
Hira Khan <sunshine9.r8@gmail.com>
2020-01-09 18:33:28 -0500

    the correct file

--- Omega2020/preprocessing.py
+++ Omega2020/preprocessing.py
@@ -22,24 +22,41 @@ class Preprocess:
 
     def pre_process_image(img, skip_dilate=False):
         """Uses a blurring function, adaptive thresholding and dilation to expose the main features of an image."""
+         # Gaussian blur with a kernal size (height, width) of 9.
+        # Note that kernal sizes must be positive and odd and the kernel must be square.
         proc = cv2.GaussianBlur(img.copy(), (9, 9), 0)
+        # Adaptive threshold using 11 nearest neighbour pixels
         proc = cv2.adaptiveThreshold(proc, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        # Invert colours, so gridlines have non-zero pixel values.
+        # Necessary to dilate the image, otherwise will look like erosion instead.
         proc = cv2.bitwise_not(proc, proc)
+        #if not skip_dilate:
+        #    # Dilate the image to increase the size of the grid lines.
+        #    kernel = np.array([[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]])
+        #    proc = cv2.dilate(proc, kernel)
         return proc
 
     def find_corners_of_largest_polygon(img):
         """Finds the 4 extreme corners of the largest contour in the image."""
-        contours, h = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
         contours = sorted(contours, key=cv2.contourArea, reverse=True)  # Sort by area, descending
         polygon = contours[0]  # Largest image
+        # Use of `operator.itemgetter` with `max` and `min` allows us to get the index of the point
+        # Each point is an array of 1 coordinate, hence the [0] getter, then [0] or [1] used to get x and y respectively.
+        # Bottom-right point has the largest (x + y) value
+        # Top-left has point smallest (x + y) value
+        # Bottom-left point has smallest (x - y) value
+        # Top-right point has largest (x - y) value
         bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
         top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
         bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
         top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+        # Return an array of all 4 points using the indices
+        # Each point is in its own array of one coordinate
         return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
     def display_points(in_img, points, radius=5, colour=(0, 0, 255)):
         """Draws circular points on an image."""
         img = in_img.copy()
+            # Dynamically change to a colour image if necessary
         if len(colour) == 3:
             if len(img.shape) == 2:
                 img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
@@ -49,6 +66,12 @@ class Preprocess:
         contours, h = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
         contours = sorted(contours, key=cv2.contourArea, reverse=True)  # Sort by area, descending
         polygon = contours[0]  # Largest image
+        # Use of `operator.itemgetter` with `max` and `min` allows us to get the index of the point
+        # Each point is an array of 1 coordinate, hence the [0] getter, then [0] or [1] used to get x and y respectively.
+        # Bottom-right point has the largest (x + y) value
+        # Top-left has point smallest (x + y) value
+        # Bottom-left point has smallest (x - y) value
+        # Top-right point has largest (x - y) value
         bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
         top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
         bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
