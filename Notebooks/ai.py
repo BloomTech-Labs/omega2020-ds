66e0ea28b85cb7b19c4c90b600a717b6e5a089d4
EC2 Default User <johana.luna87@gmail.com>
2020-01-10 05:15:55 +0000

    search function working

--- Notebooks/ai.py
+++ Notebooks/ai.py
@@ -1,57 +1,96 @@
+import copy
 
-class Sudoku():
+rows = 'ABCDEFGHI'
+cols = '123456789'
 
+boxes = [s+t for s in rows for t in cols]
+row_units = [[s+t for s in r for t in cols] for r in rows]
+column_units = [[s+t for s in rows for t in c] for c in cols]
+square_units = [[s+t for s in rs for t in cs]for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+def single_position(values):
     """
-    Sudoku class
-    ------------
-    Solves Sudoku puzzles using various techniques. Can display starting
-    position and solved attempts as a 2-D string
-
-    Parameters
-    -----------
-    puzzle(string): A sudoku puzzle in the format '8...74...1.........7.5.9.4.'
-                    where the period (.) represents an empty square
-                    puzzle string must have length of 81
-    technique(string): 'single_position', 'single_candidate'
-                        default = 'single_position'
-                        all techniques use 'single_position'
+    Go through all the boxes, and whenever there is a box with a value, eliminate this value from the values of all its peers.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
     """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit, "")
+    return values
 
-    row_values = "ABCDEFGHI"
-    column_values = "123456789"
+def single_candidate(values):
+    """
+    Go through all the units, and whenever there is a unit with a value that only fits in one box, assign the value to this box.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    for unit in unitlist:
+        for digit in "123456789":
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
-    def __init__(self, puzzle, technique='single_position'):
-
-        assert len(puzzle) == 81, "Not a valid puzzle. Must have len == 81"
-        self.puzzle = puzzle
-        self.technique = technique
-        self.combined = lambda rows, columns: [each_letter + every_number for each_letter in rows for every_number in columns]
-        self.boxes = self.combined(self.row_values, self.column_values)
+def naked_twins(values):
+    """Eliminate values using the naked twins strategy.
+    Args:
+        values(dict): a dictionary of the form {'box_name': '123456789', ...}
+    Returns:
+        the values dictionary with the naked twins eliminated from peers.
+    """
         
-        self.row_units = [self.combined(each_letter, self.column_values) for each_letter in self.row_values]
-        self.column_units = [self.combined(self.row_values, every_number) for every_number in self.column_values]
-        self.square_units = [self.combined(rs, cs) for rs in ("ABC", "DEF", "GHI") for cs in ("123", "456", "789")]
-        self.unitlist = self.row_units + self.column_units + self.square_units
-        self.units = dict((s, [u for u in self.unitlist if s in u]) for s in self.boxes)
-        self.peers = dict((s, set(sum(self.units[s], [])) - set([s])) for s in self.boxes)
-        self.values = dict(zip(self.boxes, ["123456789" if element == "." else element for element in self.puzzle]))
-        
-
-    def solver(self):
-            """
-            Solve a puzzle
+    for unit in unitlist:
+        #1. Find twins 
+        twins = [v for v in [values[box] for box in unit] if [values[box] for box in unit].count(v) == 2 and len(v) == 2]
             
-            """
-            reduce_puzzle = all(len(self.values[s]) == 1 for s in self.boxes)
-            if not reduce_puzzle:
-                return False
+        for box in unit:
+            if values[box] in twins:
+                continue
+            for twin in twins :
+                for digit in twin:
+                    values[box] = values[box].replace(digit, "")
+    return values
 
-            for unit in self.unitlist:
-                digits = '123456789'
-                for box in unit:
-                    digits = digits.replace(self.values[box], '')
-                if len(digits) != 0:
-                    return False
-            return True
+def reduce_puzzle(values):
+    """
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = single_position(values)
+        values = naked_twins(values)
+        values = single_candidate(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
-    
+def search(values):
+    "Using depth-first search and propagation, try all possible values."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+        
