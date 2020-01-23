57fe5b884c4328c27a902faf3af739cae5458559
Rob Hamilton <RobHamilton3rd@gmail.com>
2020-01-23 13:45:59 -0500

    updated to give front end coordinates

--- Omega2020/app.py
+++ Omega2020/app.py
@@ -16,7 +16,7 @@ from werkzeug.utils import secure_filename
 import urllib.request
 import sys
 import logging
-
+import re
 
 from ai import * 
 from solver import *
@@ -56,7 +56,11 @@ def create_app():
     S3_LOCATION = config('S3_LOCATION')
 
 
-
+    def find_replace_multi(string, dictionary):
+        for item in dictionary.keys():
+            # sub item for item's paired value in string
+            string = re.sub(item, dictionary[item], string)
+        return string
 
     def upload_file_to_s3(*args):
         try: s3.upload_fileobj(*args, ExtraArgs=ExtraArgs)
@@ -127,18 +131,111 @@ def create_app():
         original_grid = "."
         #import pdb; pdb.set_trace()
         grid_status = solve(pred)[0]
+
+        translation_dictionary = {
+            "A1": "00",
+            "A2": "01",
+            "A3": "02",
+            "A4": "03",
+            "A5": "04",
+            "A6": "05",
+            "A7": "06",
+            "A8": "07",
+            "A9": "08",
+            "B1": "10",
+            "B2": "11",
+            "B3": "13",
+            "B4": "14",
+            "B5": "15",
+            "B6": "15",
+            "B7": "16",
+            "B8": "17",
+            "B9": "18",
+            "C1": "20",
+            "C2": "21",
+            "C3": "23",
+            "C4": "23",
+            "C5": "24",
+            "C6": "25",
+            "C7": "26",
+            "C8": "27",
+            "C9": "28",
+            "D1": "30",
+            "D2": "31",
+            "D3": "32",
+            "D4": "33",
+            "D5": "34",
+            "D6": "35",
+            "D7": "36",
+            "D8": "37",
+            "D9": "38",
+            "E1": "40",
+            "E2": "41",
+            "E3": "42",
+            "E4": "43",
+            "E5": "44",
+            "E6": "45",
+            "E7": "46",
+            "E8": "47",
+            "E9": "48",
+            "F1": "50",
+            "F2": "51",
+            "F3": "52",
+            "F4": "53",
+            "F5": "54",
+            "F6": "55",
+            "F7": "56",
+            "F8": "57",
+            "F9": "58",
+            "G1": "60",
+            "G2": "61",
+            "G3": "62",
+            "G4": "63",
+            "G5": "64",
+            "G6": "65",
+            "G7": "66",
+            "G8": "67",
+            "G9": "68",
+            "H1": "70",
+            "H2": "71",
+            "H3": "72",
+            "H4": "73",
+            "H5": "74",
+            "H6": "75",
+            "H7": "76",
+            "H8": "77",
+            "H9": "78",
+            "I1": "80",
+            "I2": "81",
+            "I3": "82",
+            "I4": "83",
+            "I5": "84",
+            "I6": "85",
+            "I7": "86",
+            "I8": "87",
+            "I9": "88",
+        }
         if grid_status == 'Ivalid Sudoku, check these values:':
-            solved_grid = "Invalid Puzzle"
-            solved = "Invalid Puzzle"
+            grid_status = "Invalid"
+            errors = list(solve(pred))
+            errors.pop(0)
+            solution = []
+            for e in errors:
+                guess_pair = []
+                guess = e[0] 
+                cell = find_replace_multi(e[1],translation_dictionary)
+                guess_pair.append(guess)
+                guess_pair.append(cell)
+                solution.append(guess_pair) 
         else:
             solved_grid = solve(pred)[2]
             #import pdb; pdb.set_trace()
             solved = display(solved_grid)
-
-
+            solution = solve(pred)
+        
         
         #return render_template('results.html', imghash = imghash, imgurl = imgurl, pred=pred, processed_url=processed_url, processed_cells=processed_cells,original_grid=original_grid,solved=solved)
-        return jsonify(values = pred, puzzle_status=grid_status, solution=solve(pred)[1])
+        return jsonify(values = pred, puzzle_status=grid_status, solution=solve(pred))
 
 
 
